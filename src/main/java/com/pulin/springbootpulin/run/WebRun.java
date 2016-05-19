243834cc410d906e3e67d845c8bebb07adeed156
pul@shishike.com <pul@shishike.com>
2016-05-19 10:49:59 +0800

    update

--- src/main/java/com/pulin/springbootpulin/run/WebRun.java
+++ src/main/java/com/pulin/springbootpulin/run/WebRun.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
 import java.util.TreeMap;
 
 @Controller
