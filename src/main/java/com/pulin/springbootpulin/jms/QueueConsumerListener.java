195445a3d142cf8776a36787ed2f9fe848484e2d
pul <273471249@qq.com>
2016-07-07 09:00:38 +0800

    update

--- src/main/java/com/pulin/springbootpulin/jms/QueueConsumerListener.java
+++ src/main/java/com/pulin/springbootpulin/jms/QueueConsumerListener.java
@@ -1,18 +1,23 @@
 package com.pulin.springbootpulin.jms;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.TextMessage;
 
+import org.apache.activemq.command.ActiveMQTextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QueueConsumerListener implements MessageListener {
 	final static Logger logger = LoggerFactory.getLogger(QueueConsumerListener.class);
 	public void onMessage(Message message) {
-		if(message instanceof TextMessage){
-			 TextMessage msg = (TextMessage) message;
-			 logger.info("gateway-jms-cosumer-test,msg:{}",msg.toString());
+		if(message instanceof ActiveMQTextMessage){
+			ActiveMQTextMessage msg = (ActiveMQTextMessage) message;
+			 try {
+				logger.info("gateway-jms-cosumer-test,msg:{}",msg.getText().toString());
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,7 +1,41 @@
 package com.test;
 
-/**
- * Created by MyPC on 2016/5/25.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
+	
+	public static class MyThread extends Thread implements Runnable{
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	public static void main(String[] args) {
+		long s = System.currentTimeMillis();
+		try{
+			List list = new ArrayList();
+			while (true) {
+				//list.add(new Test());
+				new MyThread().start();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}	
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			long e = System.currentTimeMillis();
+			System.out.println(e-s);
+		}
+	
+	}
 }
